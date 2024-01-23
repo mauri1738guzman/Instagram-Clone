6b61975d82ff00cf4671ee91639c5a9e536cdea5
Mauricio Perez <mauri_guzman@yahoo.com>
2024-01-23 16:03:38 -0600

    youtube time

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AuthPage from "./pages/AuthPage/AuthPage";
 import PageLayout from "./Layouts/PageLayout/PageLayout";
 function App() {
 
-//-- Time of video: 56
+//-- Time of video: 1:07:02
 //-- URL: https://www.youtube.com/watch?v=RMScMwY2B6Q
 //-- running app: run command 
 //-- 1) npm run dev
