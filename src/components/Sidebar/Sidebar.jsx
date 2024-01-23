9fb60b17b26465bcc7669cc4527c56fc2893f845
Mauricio Perez <mauri_guzman@yahoo.com>
2024-01-23 15:59:42 -0600

    Finished Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,45 +1,130 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
-import {  Link as RouterLink } from 'react-router-dom';
-import { InstagramLogo, InstagramMobileLogo } from '../../assets/constants';
-
+import { Avatar, Box, Flex, Link, Tooltip } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { InstagramLogo, InstagramMobileLogo, SearchLogo, NotificationsLogo, CreatePostLogo } from '../../assets/constants';
+import { AiFillHome } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi"
 
 const Sidebar = () => {
-  return <Box 
-    height={"100vh"}
-    borderRight={"1px solid"}
-    borderColor={"whiteAlpha.300"}
-    py={8}
-    position={"sticky"}
-    top={0}
-    left={0}
-    px={{base:2, md:4}}>
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo />,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: "Create",
+    },
+    {
+      icon: <Avatar size={"sm"} name='Burak Orkmez' src='/profilepic.png' />,
+      text: "Profile",
+      link: "/asaprogrammer",
+    },
+  ];
 
-
-
-  <Flex direction={"column"} gap={10} w={"full"} height={"full"} >
-
-
-        {/* This is for both the home logo in mobile and desktop site */}
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none", md:"block"}} cursor="pointer">
-        <InstagramLogo/>
+  return (
+    <Box
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
+        <Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor="pointer">
+          <InstagramLogo />
         </Link>
 
-        <Link to={"/"} as={RouterLink} p={2} display={{base:"block", md:"none"}} 
-        borderRadius={6}
-        hover={{
-          bg:"whiteAlpha.200",
-        }}
-        w={10}
-        cursor='pointer'>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          p={2}
+          display={{ base: "block", md: "none" }}
+          borderRadius={6}
+          hover={{
+            bg: "whiteAlpha.200",
+          }}
+          w={10}
+          cursor='pointer'
+        >
+          <InstagramMobileLogo />
+        </Link>
 
-        <InstagramMobileLogo/>
-      </Link>
+        <Flex direction={"column"} gap={5} cursor={"pointer"}>
+        {sidebarItems.map((item, index) => (
+  <Tooltip
+    key={index}
+    hasArrow
+    label={item.text}
+    placement='right'
+    ml={1}
+    openDelay={500}
+    display={{ base: 'block', md: 'none' }}
+  >
+    <Link
+      display={"flex"}
+      to={item.link || null}
+      as={RouterLink}
+      alignContent={"center"}
+      gap={4}
+      _hover={{ bg: "whiteAlpha.400" }}
+      borderRadius={6}
+      p={2}
+      w={{base: 10, md: "full"}}
+      justifyContent={{base:"center", md:"flex-start"}}
+
+      
+    >
+      {item.icon}
+      <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
+    </Link>
+  </Tooltip>
+))}
 
 
+        </Flex>
+        <Tooltip
+   
+    hasArrow
+    label={"Logout"}
+    placement='right'
+    ml={1}
+    openDelay={500}
+    display={{ base: 'block', md: 'none' }}
+  >
+    <Link
+      display={"flex"}
+      to={"/auth"}
+      as={RouterLink}
+      alignContent={"center"}
+      gap={4}
+      _hover={{ bg: "whiteAlpha.400" }}
+      borderRadius={6}
+      p={2}
+      w={{base: 10, md: "full"}}
+      justifyContent={{base:"center", md:"flex-start"}}
+      mt={'auto'}
+
+      
+    >
+      <BiLogOut size={25} />
+      <Box display={{ base: "none", md: "block" }}>Logout</Box>
+    </Link>
+  </Tooltip>
       </Flex>
-
     </Box>
-  
+  );
 };
 
-export default Sidebar
+export default Sidebar;
